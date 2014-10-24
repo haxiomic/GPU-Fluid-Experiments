3729e41348a2c6846cef1a443a1d00455cf68f6a
haxiomic <haxiomic@gmail.com>
2014-10-24 11:22:11 +0100

    Fixed edge case ios glsl issue

--- Source/shaders/glsl/geom.glsl
+++ Source/shaders/glsl/geom.glsl
@@ -7,7 +7,7 @@ float distanceToSegment(vec2 a, vec2 b, vec2 p, out float projection){
 
 	if(projection < 0.0)            return length(d);
 	else if(projection > length(x)) return length(p - b);
-	else                            return sqrt(abs(dot(d,d) - projection*projection));
+	return sqrt(abs(dot(d,d) - projection*projection));
 }
 float distanceToSegment(vec2 a, vec2 b, vec2 p){
 	float projection;
